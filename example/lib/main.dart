3fffc79316dbd8218814c5c2843a3d1a8db72646
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2021-10-19 11:36:27 +0530

    Update example & equivalent Dart code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:windows_taskbar/windows_taskbar.dart';
 
 void main() {
@@ -16,34 +13,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await WindowsTaskbar.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    WindowsTaskbar.setThumbnailToolbar(
+      [
+        ThumbnailToolbarButton(
+          ThumbnailToolbarAssetIcon('res/folder.ico'),
+          'Button 1',
+          () {},
+        ),
+        ThumbnailToolbarButton(
+          ThumbnailToolbarAssetIcon('res/pictures.ico'),
+          'Button 2',
+          () {},
+        ),
+        ThumbnailToolbarButton(
+          ThumbnailToolbarAssetIcon('res/sync.ico'),
+          'Button 3',
+          () {},
+        ),
+      ],
+    );
   }
 
   @override
@@ -51,11 +42,9 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('windows_taskbar'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+        body: const Center(),
       ),
     );
   }
--- lib/windows_taskbar.dart
+++ lib/windows_taskbar.dart
@@ -1,13 +1,125 @@
+/*
+ *  This file is part of windows_taskbar
+ * (https://github.com/alexmercerind/windows_taskbar).
+ *
+ *  windows_taskbar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  windows_taskbar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with windows_taskbar. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2021, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+ */
 
-import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:path/path.dart';
 
-class WindowsTaskbar {
-  static const MethodChannel _channel = MethodChannel('windows_taskbar');
+const String _kSetProgressMode = "SetProgressMode";
+const String _kSetProgress = "SetProgress";
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+/// Sets thumbnail toolbar.
+const String _kSetThumbnailToolbar = "SetThumbnailToolbar";
+
+/// Method channel for making native WIN32 calls.
+const MethodChannel _kChannel =
+    MethodChannel('com.alexmercerind/windows_taskbar');
+
+/// Declares mode of a [ThumbnailToolbarButton].
+/// Analog of `THUMBBUTTONFLAGS` in WIN32 API.
+///
+class ThumbnailToolbarButtonMode {
+  static const int enabled = 0;
+  static const int disabled = 0x1;
+  static const int dismissionClick = 0x2;
+  static const int noBackground = 0x4;
+  static const int hidden = 0x8;
+  static const int nonInteractive = 0x10;
+}
+
+/// Helper class to retrieve path of the icon asset.
+class ThumbnailToolbarAssetIcon {
+  /// Asset location of the `*.ico` file.
+  final String asset;
+
+  String get path => join(
+        dirname(Platform.resolvedExecutable),
+        'data',
+        'flutter_assets',
+        asset,
+      );
+
+  ThumbnailToolbarAssetIcon(this.asset) {
+    assert(
+      Platform.isWindows && asset.endsWith('.ico') && File(path).existsSync(),
+    );
   }
 }
+
+/// Represents a thumbnail toolbar button.
+///
+class ThumbnailToolbarButton {
+  /// [File] to the icon of the button. Must be `*.ico` format.
+  final ThumbnailToolbarAssetIcon icon;
+
+  /// Tooltip of the button. Showed upon hover.
+  final String tooltip;
+
+  /// Display configuration of the button. See [ThumbnailToolbarButtonMode] for more information.
+  final int mode;
+
+  /// Called when button is clicked from the toolbar.
+  final void Function() onClick;
+
+  /// Internally served id of the button.
+  late int id;
+
+  ThumbnailToolbarButton(
+    this.icon,
+    this.tooltip,
+    this.onClick, {
+    this.mode = ThumbnailToolbarButtonMode.enabled |
+        ThumbnailToolbarButtonMode.dismissionClick,
+  }) {
+    id = _thumbnailToolbarButtonId++;
+  }
+
+  /// Conversion to `flutter::EncodableMap` for method channel transfer.
+  Map<String, dynamic> toJson() => {
+        'icon': icon.path,
+        'tooltip': tooltip,
+        'mode': mode,
+        'id': id,
+      };
+}
+
+/// WindowsTaskbar
+/// --------------
+///
+/// Flutter plugin serving utilities related to Windows taskbar.
+///
+class WindowsTaskbar {
+  /// Sets thumbnail toolbar for the taskbar app icon.
+  /// Takes list of thumbnail toolbar buttons.
+  ///
+  static Future<void> setThumbnailToolbar(
+      List<ThumbnailToolbarButton> buttons) async {
+    await _kChannel.invokeMethod(
+      _kSetThumbnailToolbar,
+      {
+        'buttons': buttons.map((button) => button.toJson()).toList(),
+      },
+    );
+  }
+}
+
+/// Global index for creating new thumbnail toolbar buttons & assigning unique id.
+int _thumbnailToolbarButtonId = 40001;
